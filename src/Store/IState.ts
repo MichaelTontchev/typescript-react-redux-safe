8022fd4871767257aa0f576a3122b6c97e203193
Michael Tontchev <michael.tontchev@yahoo.com>
2017-05-25 09:55:21 -0700

    Add interface

--- src/Store/IState.ts
+++ src/Store/IState.ts
@@ -1,12 +1,14 @@
 import { Immutable, ImmutableArray } from 'seamless-immutable';
 
 export interface IState {
-	myStuff: {
-		clickCounts: number,
-		websiteProperties: IWebsiteProperties,
-	};
+	myStuff: IMyStuff;
 }
 
 export interface IWebsiteProperties {
 	name: string;
+}
+
+export interface IMyStuff {
+	clickCounts: number;
+	websiteProperties: IWebsiteProperties;
 }
